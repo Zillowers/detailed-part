204edf80f3a275c34047f737006001c4a14535f1
Alex Burkowsky <aburkowsky9@gmail.com>
2018-11-24 12:31:41 -0800

    add compression package for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+// const CompressionPlugin = require('compression-webpack-plugin');
 
 const SRC_DIR = path.join(__dirname, '/client/src');
 const DIST_DIR = path.join(__dirname, '/client/dist');
@@ -10,13 +11,22 @@ module.exports = {
     path: DIST_DIR,
   },
   module: {
+    // plugins: [
+    //   new CompressionPlugin({
+    //     filename: '[path].gz[query]',
+    //     algorithm: 'gzip',
+    //     test: /\.js$|\.css$|\.html$/,
+    //     threshold: 10240,
+    //     minRatio: 0.8,
+    //   }),
+    // ],
     loaders: [
       {
         test: /\.jsx?/,
         include: SRC_DIR,
-        loader: 'babel-loader',      
+        loader: 'babel-loader',
         query: {
-          presets: ['react', 'es2015']
+          presets: ['react', 'es2015'],
         },
       },
       {
