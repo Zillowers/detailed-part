28695117f9cc3ed135bb478a387043f7b3fd3625
Alex Burkowsky <aburkowsky9@gmail.com>
2018-11-28 14:31:50 -0800

    add dotenv configs

--- sdc_db/index.js
+++ sdc_db/index.js
@@ -1,9 +1,25 @@
 const cassandra = require('cassandra-driver');
+require('dotenv').config();
 
-const client = new cassandra.Client({
-  contactPoints: ['localhost:9042'],
-  keyspace: 'houses',
-});
+const { types: distance } = cassandra;
+
+const options = {
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  contactPoints: [
+    process.env.DB_CONTACT1,
+    process.env.DB_CONTACT2,
+    process.env.DB_CONTACT3,
+  ],
+  keyspace: process.env.DB_KEYSPACE,
+  pooling: {
+    setMaxRequestsPerConnection: {
+      [distance.local]: 32768,
+      [distance.remote]: 2048,
+    },
+  },
+};
+const client = new cassandra.Client(options);
 client.connect((err) => {
   if (err) {
     console.log('CONNECTION ERROR: ', err);
